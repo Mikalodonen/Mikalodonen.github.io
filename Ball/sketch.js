77eb2df8227195ebca07e9ef5404d1a6ab7f87e1
mikkel <mikkelandersen295@gmail.com>
2021-09-22 19:19:56 +0200

    trying to fix the ball

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -1,22 +1,22 @@
-
 function setup() {
-  createCanvas(400, 400); 
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
-
+  background(220);
 }
-{let y = 0;
-  let speed=1;
-  let gravity=0.5;}
+let y = 0;
+let speed=1;
+let gravity=0.2;
 
-  function draw(){ 
-    ellipse(200,y,20,20);
-    y=y+speed;
-    speed=speed+gravity;
-    
-    if(y>400){
-    //reverse the speed
-      speed=-0.75 * speed;
-    }
+
+function draw(){ 
+  ellipse(200,y,20,20);
+  y=y+speed;
+  speed=speed+gravity;
+  
+  if(y>400){
+  //reverse the speed
+    speed=-0.95 * speed;
+  }
+}
