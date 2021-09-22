4918165ac745c01e54fcc516b31e8d681703071c
mikkel <mikkelandersen295@gmail.com>
2021-09-22 19:14:59 +0200

    added ball

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -1,8 +1,22 @@
+
 function setup() {
   createCanvas(400, 400); 
 }
 
 function draw() {
   background(255);
-  
+
 }
+{let y = 0;
+  let speed=1;
+  let gravity=0.5;}
+
+  function draw(){ 
+    ellipse(200,y,20,20);
+    y=y+speed;
+    speed=speed+gravity;
+    
+    if(y>400){
+    //reverse the speed
+      speed=-0.75 * speed;
+    }
