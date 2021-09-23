8f6664bd8898e7b38ae0e0713dc2a6d6a3591816
mikkel <mikkelandersen295@gmail.com>
2021-09-23 10:48:47 +0200

    ball

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -4,13 +4,13 @@ function setup() {
 
 function draw() {
   background(255);
-}
+
 let y = 0;
 let speed=1;
 let gravity=0.5;
 
 
-function draw(){ 
+function draw()
   ellipse(200,y,20,20);
   y=y+speed;
   speed=speed+gravity;
