e791af565b45e693a77248f6f775546b988a7018
mikkel <mikkelandersen295@gmail.com>
2021-09-23 12:50:56 +0200

    Ball fixed

--- Ball/sketch.js
+++ Ball/sketch.js
@@ -2,20 +2,18 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function draw() {
-  background(255);
-}
 let y = 0;
 let speed=1;
 let gravity=0.5;
 
 
 function draw(){ 
+  background(255);
   ellipse(200,y,20,20);
   y=y+speed;
   speed=speed+gravity;
   
-  if(y>400){
+  if(y>389){
   //reverse the speed
     speed=-0.75 * speed;
   }
