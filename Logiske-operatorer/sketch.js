280b3e81dade6b718a7f78560878abaee6f0fd2a
mikkel <mikkelandersen295@gmail.com>
2021-10-06 21:31:52 +0200

    Circle movement

--- Logiske-operatorer/sketch.js
+++ Logiske-operatorer/sketch.js
@@ -1,8 +1,13 @@
 function setup() {
-  createCanvas(400, 400); 
-}
+  createCanvas(400, 400);  
+}  
 
 function draw() {
-  background(255);
-  
+background(255);  
+
+  stroke(0);
+  fill(225,0,0);
+  ellipse(mouseX,mouseY,40,40);
 }
+
+
