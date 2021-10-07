c4434e2f94f548b522b953c5deef916948d3800e
mikkel <mikkelandersen295@gmail.com>
2021-10-07 14:39:03 +0200

    yes

---
+++ Løkker/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 500);
+  text("Output is in the console", 20,20)
+
+  //opgave 1
+console.log("loop demo opgave 1");
+
+//initialization
+let i = 10 
+
+while(i > 0){
+  console.log("while demo" +i)
+
+  //increment
+  i=i-1
+}
+console.log("done");
+
+//opgave 2
+
+
+}
