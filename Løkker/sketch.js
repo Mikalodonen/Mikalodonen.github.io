ae6d9a104d4d73f667185eb7b047b8216570b558
mikkel <mikkelandersen295@gmail.com>
2021-10-14 14:17:32 +0200

    opg 2 3 4 osv virker

--- Løkker/sketch.js
+++ Løkker/sketch.js
@@ -18,8 +18,40 @@ console.log("done");
 
 //opgave 2
 
+console.log("loop demo opgave 2");
+let xSum = 0
+for(let x = 1; x <= 20; x++){
+//xSum = xSum + x
+xSum += x
 
-for(let i = 0; i < 10; i++){
-  console.log(i);
+console.log(xSum)
+
 }
+console.log("done");
+
+//opgave 3
+
+console.log("loop demo opgave 2");
+
+for(let n = 1; n <= 10; n++){
+  console.log(n+"-tabellen")
+
+  for(let multi=1; multi<=10; multi++){
+  let result = n * multi
+  console.log(result)
+
+//console.log(2-tabellen)
+//for(let i=2; i<=20; i+= 2){
+//console.log(i)
+
+//console.log(3-tabellen)
+//for(let i=3; i<=30; i+= 3){
+//console.log(i)
+}
+}
+console.log("done");
+
+//opgave 4
+
+
 }
